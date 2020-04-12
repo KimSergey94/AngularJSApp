72f46a39c25aff14b9a8f7d81999599a341526dc
KimSergey94 <sksss060213@gmail.com>
2020-04-12 14:21:55 +0600

    finished 2 parts

---
+++ AngularApp/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+using AngularApp.Models.EF;
+
+namespace AngularApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private AngularAppDbContext db = null;
+
+        public UserController()
+        {
+            db = new AngularAppDbContext();
+        }
+
+        public JsonResult Index()
+        {
+            var users = db.Users;
+            return Json(users, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Details(int id)
+        {
+            var user = db.Users.Find(id);
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Create(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return Json(null);
+        }
+
+        [HttpPost]
+        public JsonResult Edit(User user)
+        {
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Json(null);
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            var user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return Json(null);
+        }
+    }
+}
---
+++ AngularApp/Models/EF/AngularAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AngularApp.Models.EF
+{
+    public class AngularAppDbContext : DbContext
+    {
+        public AngularAppDbContext() : base("name = AngularAppDbContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
